293c21475eea9bd69464f67a794d64cf76634ab4
Dinis <khafizov.di@phystech.edu>
2023-05-13 12:26:46 +0300

    i'm done

--- src/IterationMethods/Gradient/CG.cpp
+++ src/IterationMethods/Gradient/CG.cpp
@@ -11,6 +11,27 @@ std::vector<double> CGD(const CSR &A, const std::vector<double> &x_0, const std:
         koef2 = r * r;
         d = r + d * koef2/koef1;
         koef1 = koef2; 
+    }
+    return x;
+}
+std::vector<double> CGD_precond(const CSR &A, const std::vector<double> &x_0, const std::vector<double> &b, double tolerance) {
+    std::vector<double> x = x_0, r = A * x_0 - b;
+    std::vector<double> w = CGD(A, x_0, r, tolerance);
+    std::vector<double> d = w, r_last, w_last;
+    double alpha, beta, norm = first_norm(r);
+
+    while(norm > tolerance) {
+        r_last = r;
+        w_last = w;
+        alpha = (r * w)/(d * (A * d));
+        x = x - alpha * d;
+        r = A * x - b;
+        norm = first_norm(r);
+        if (norm > tolerance) {
+            w = CGD(A, x_0, r, tolerance);
+            beta = (r * w)/(r_last * w_last);
+            d = w + beta * d;
         }
+    }
     return x;
 }
--- src/IterationMethods/Gradient/CG.hpp
+++ src/IterationMethods/Gradient/CG.hpp
@@ -7,5 +7,6 @@
 #include "Vect/VectorOperations.hpp"
 
 std::vector<double> CGD(const CSR &A, const std::vector<double> &x_0, const std::vector<double> &b, double tolerance);
+std::vector<double> CGD_precond(const CSR &A, const std::vector<double> &x_0, const std::vector<double> &b, double tolerance);
 
 #endif
